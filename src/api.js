9defa1a74bb87c29c90ad374c04eda7266f172fe
Willem Imac <willem@scubed.co.za>
2018-02-12 17:03:46 +0200

    Remove Entry based API.
    Make correlation between webpack config and API stronger.

--- src/api.js
+++ src/api.js
@@ -1,9 +1,30 @@
-var Entry = require('./entry.js')
+// var Entry = require('./entry.js')
 
 var api = {
-    _entries: [],
-    entry: function(src) {
-        return this.entries.push(new Entry(src));
+    _entry: {},
+    _extracts: [],
+    _extractCSS: [],
+    _css: [],
+
+    entry: function(entry) {
+        this._entry = entry;
+    },
+    extract: function(modules, name) {
+        this._extracts.push({
+            modules,
+            name
+        })
+    },
+    extractCSS: function(modules, name) {
+        this._extractCSS.push({
+            modules, 
+            name
+        })
+    },
+    css: function(entry) {
+        // a css entry just like entry, but will only generate a CSS file 
+        // not a JS file combined
+        this._css.push(entry);
     }
 }
 
--- src/webpackConfig.js
+++ src/webpackConfig.js
@@ -1,7 +1,47 @@
 // Generate webpack config file with an instance of an API object
 
 export default function(api) {
+    var config =  {
+        entry: api._entry,
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: '[name].js',
+            publicPath: '/webpack/'
+        },
+        devServer: {
+            overlay: true,
+            inline: true,
+            hot: true
+        },
+        module: {
+            rules: []
+        },
+        plugins: []
+    }
 
+    if (api.config.analyze) {
+        config.plugins.push(new BundleAnalyzerPlugin({
+            analyzerMode: 'static'
+        }))
+    }
 
+    // All base entries
+    api._extracts.forEach(e => {
+        // e.modules, e.name
+        
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'lib',
+        //     chunks: ['main', 'a', 'b'],
+        //     // children: true,
+        //     minChunks(module, count) {
+        //         // module.resource = /Users/willem/projects/webpackdotnet/src/index.js 
+        //         // module.context = /Users/willem/projects/webpackdotnet/src
+        //         return module.resource.indexOf('lib') !== -1
+        //     },
+        // }),
+
+        
+    });
     
+    return config
 }
