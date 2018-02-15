a07c2ab8a3fc57a626e63f572974fc6cd0cddace
Willem Imac <willem@scubed.co.za>
2018-02-15 09:13:39 +0200

    Include webpack.js from host project

--- src/dev.js
+++ src/dev.js
@@ -1,21 +1,18 @@
-console.log('Dev ran!')
 // // Run dev server with options provided by host app using API
-// var path = require('path')
+var path = require('path')
 
-// function getHostFile(file) {
-//     // In dev mode use symbolic link path rather then actual path
-//     return path.resolve(path.resolve(path.join('/Users/Willem/temp/usedotnetwebpack', file)))
+function getHostFile(file) {
+    // In dev mode use symbolic link path rather then actual path
+    return path.resolve(path.join('/Users/Willem/temp/usedotnetwebpack', file))
 
-//     // This file is located in in node_modules/usedotnetwebpack/src/api.js
-//     // We want the source of the original project.
-//     // Need to move up three directories
-//     return path.resolve(path.join(__dirname, '../../../', file));
-// }
+    // This file is located in in node_modules/usedotnetwebpack/src/api.js
+    // We want the source of the original project.
+    // Need to move up three directories
+    return path.resolve(path.join(__dirname, '../../../', file))
+}
 
-// // inlcude api from host application
-
-// var host = getHostFile();
-// var apiFile = path.resolve(path.join(host, 'webpack.js'))
-// console.log('Api file', apiFile)
-
-
+// include api from host application
+var configFile = getHostFile('webpack.js')
+console.log('This is the config file', configFile)
+var config = require(configFile)
+console.log('Api file', config)
