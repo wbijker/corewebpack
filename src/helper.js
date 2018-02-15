e26e7ccd5d0ec89ff81ddb442ae4a145f87911a0
Willem Imac <willem@scubed.co.za>
2018-02-15 17:19:23 +0200

    Commit dev test path

--- src/helper.js
+++ src/helper.js
@@ -3,7 +3,10 @@ var path = require('path')
 // This file is located in in node_modules/usedotnetwebpack/src/helper.js
 // We want the source of the original project.
 // Need to move up three directories
-const BASE = path.join(__dirname, '../../../')
+// const BASE = path.join(__dirname, '../../../')
+
+// In dev mode use symbolic link path rather then actual path
+const BASE = '/Users/willem/Temp/usedotnetwebpack'
 
 module.exports = {
     projectPath: function(file) {
