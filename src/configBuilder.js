59db745fdb9c040e0a5f9a64a99791699fef5ca6
Willem Imac <willem@scubed.co.za>
2018-02-19 11:33:06 +0200

    Combine rules and plugins in one file. Add webpack manifest plugin

--- src/configBuilder.js
+++ src/configBuilder.js
@@ -1,11 +1,78 @@
 // Generate webpack config file with an instance of an API object
 
-var Webpack = require('webpack')
-var DefaultRules = require('./rules')
-var Helper = require('./helper')
-var Plugins = require('./plugins')
+const Webpack = require('webpack')
+const Path = require('path')
+const ManifestPlugin = require('webpack-manifest-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const Helper = require('./helper')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
-module.exports = function(api) {
+function isIncluded(module, entry) {
+    if (entry.charAt(0) == '.') {
+        // File
+        // console.log('Relative library')
+        // console.log(Helper.projectPath(entry))
+        return Helper.projectPath(entry) === module.resource
+    }
+    // Module
+    var rp = Helper.projectPath(Path.join('node_modules', entry))
+    return module.context.indexOf(Helper.projectPath(Path.join('node_modules', entry))) === 0
+}
+
+
+function createCommonChunk(entry) {
+    var obj = {
+        name: entry.name,
+        chunks: entry.chunks || null,
+        minChunks: 2
+    }
+
+    if (entry.modules && entry.modules.length > 0) {
+        obj.minChunks = function(module, count) {
+            // return true if some of the modules is included in path
+            return entry.modules.some(e => isIncluded(module, e))
+        }
+    }
+
+    return new Webpack.optimize.CommonsChunkPlugin(obj)
+}
+
+function generateRules(api) {
+    var rules =  [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    cacheDirectory: true,
+                    presets: ['env']
+                }
+            }
+        },
+        {
+            test: /\.vue$/,
+            loader: 'vue-loader',
+            options: {
+                loaders: {
+                    js: 'babel-loader'
+                }
+            }
+        },
+    ]
+
+    rules.concat(rules, api._extractCSS.map(e => 
+        // extract common CSS into seperate chunks
+        ({    
+            // maybe rule.Condition?
+            test: e.test,
+            use: extractCSS.extract([ 'css-loader', 'postcss-loader' ])
+        })
+    ))
+    return rules
+}
+
+function generatePlugins(api) {
     var plugins = []
     if (api._dev) {
         plugins.push(new Webpack.HotModuleReplacementPlugin())
@@ -19,18 +86,26 @@ module.exports = function(api) {
         }))
     }
 
-    var rules = DefaultRules(api)
-    // Add custom rules added by API
-    if (api._rules.length > 0) {
-        rules = rules.concat(api._rules)
-    }
-    // Add plugins needed
-    Plugins(api, plugins)
-    // Add custom plugins provided by API interface
-    if (api._plugins.lenght > 0) {
-        plugins = plugins.concat(api._plugins)
+    if (api._config.analyzeBundle) {
+        plugins.push(new BundleAnalyzerPlugin({analyzerMode: 'static'}))
     }
 
+    // Add commonChunks plugin
+    api._extracts.forEach(entry => plugins.push(createCommonChunk(entry)))
+
+    if (api.extractCSS.length > 0) {
+        plugins.push(new ExtractTextPlugin({
+            filename: 'style.css'
+        }))
+    }
+
+    // Add manifest plugin for linking server side with client side 
+    // Will generate manifest.json
+    plugins.push(new ManifestPlugin())
+    return plugins    
+}
+
+module.exports = function(api) {
     var config =  {
         entry: api._entries,
         output: {
@@ -48,10 +123,9 @@ module.exports = function(api) {
             }
         },
         module: {
-            rules: rules,
+            rules: generateRules(api),
         },
-        plugins: plugins
+        plugins: generatePlugins(api)
     }
-
     return config
 }
